d1b131ef313908f0d8d75d3719ea751d9cd69875
Arinze Owoh <arinzeowoh@gmail.com>
2024-06-17 06:07:05 +0100

    change base to match new name of repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/From-Figma-To-Code-Final-Challenge-Solution/"
+  base:"/Drone-Racing-Landing-Page/"
 })
